ead3853365119f9b4b5200d53a2f3e12dc1cb1cb
Marcos Vinicius da Cunha Brito <marcosvcbrito@gmail.com>
2019-12-05 21:01:04 -0300

    first project

---
+++ musicChallengeTW/musicChallengeTW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  musicChallengeTW
+//
+//  Created by Marcos Vinicius da Cunha Brito on 05/12/19.
+//  Copyright © 2019 Marcos Vinicius da Cunha Brito. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
